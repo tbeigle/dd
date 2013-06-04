f96dd9a0fce5cec267225f268aad111a8cf717b2
Tom Beigle <tom@oho.com>
2013-06-04 17:23:50 -0400

    Hiding the OHO logo.

--- docroot/sites/all/themes/oho_basic_responsive/templates/page/page.tpl.php
+++ docroot/sites/all/themes/oho_basic_responsive/templates/page/page.tpl.php
@@ -74,11 +74,11 @@
     
       <div class="section row clearfix">
 
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
+      <?php //if ($logo): ?>
+        <!--<a href="<?php //print $front_page; ?>" title="<?php //print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php //print $logo; ?>" alt="<?php //print t('Home'); ?>" />
+        </a>-->
+      <?php //endif; ?>
 
       <?php if ($site_name || $site_slogan): ?>
         <div id="name-and-slogan">
