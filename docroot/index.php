a752c58269ffcc2f45fff91a5db14508b898147c
Tom Beigle <tbeigle@gmail.com>
2014-04-15 09:53:50 -0500

    Added tracking code.

--- docroot/index.php
+++ docroot/index.php
@@ -4,6 +4,15 @@
     <title>Designated Developers | Site Coming Soon</title>
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
     <link href="/styles/css/rebuild-lp.css" rel="stylesheet">
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      
+      ga('create', 'UA-44636522-1', 'designated-developers.com');
+      ga('send', 'pageview');
+    </script>
   </head>
   <body>
     <img class="rebuild-logo" src="/sites/all/themes/dd/images/logo-watermark.png">
