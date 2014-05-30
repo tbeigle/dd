0286c63abe0cee823d52baa67b009c77f8737516
Tom Beigle <tbeigle@gmail.com>
2014-05-30 12:57:53 -0500

    version-1.0: Adding the new version of the index file.

--- docroot/index.php
+++ docroot/index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 9]> <html class="lt-ie-9 no-js"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html dir="ltr" lang="en-US" class="no-js"><!--<![endif]-->
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Designated Developers | Site Coming Soon</title>
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
-    <link href="/styles/css/rebuild-lp.css" rel="stylesheet">
+    <link href="/styles/css/dd.css" rel="stylesheet">
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -14,8 +16,110 @@
       ga('send', 'pageview');
     </script>
   </head>
-  <body>
-    <img class="rebuild-logo" src="/sites/all/themes/dd/images/logo-watermark.png">
-    <h1 class="rebuild-copy">Website Launching <strong>May, 2014</strong></h1>
+  <body class="no-overlay">
+    <nav id="popout-nav" class="container">
+      <p id="popout-nav-toggle-wrapper" class="block-auto">
+        <a id="popout-nav-toggle" class="glyph no-scroll" href="#overlay-menu" tabindex="1">i</a>
+      </p> <!-- /#popout-nav-toggle-wrapper /.block-auto -->
+      
+      <nav id="fixed-menu-buttons">
+        <ul class="menu-buttons">
+          <li><a class="button-link main-nav-link portfolio active scroll-link" href="#portfolio-container" tabindex="2">Portfolio</a></li>
+          <li><a class="button-link main-nav-link wwd scroll-link" href="#wwd-container" tabindex="3">What We Do</a></li>
+          <li><a class="button-link main-nav-link wwa scroll-link" href="#wwa-container" tabindex="4">Who We Are</a></li>
+          <li><a class="button-link main-nav-link ttu scroll-link" href="#ttu-container" tabindex="5">Talk To Us</a></li>
+        </ul> <!-- /.menu-buttons -->
+      </nav> <!-- /#fixed-menu-buttons -->
+      
+      <div id="overlay-menu">
+        <div class="inner">
+          <a class="close-me no-scroll" title="Click to close." href="#overlay-menu">x</a>
+          
+          <ul class="menu">
+            <li><a class="menu-link main-nav-link portfolio scroll-link" href="#portfolio-container">Portfolio</a></li>
+            <li><a class="menu-link main-nav-link wwd scroll-link" href="#wwd-container">What We Do</a></li>
+            <li><a class="menu-link main-nav-link wwa scroll-link" href="#wwa-container">Who We Are</a></li>
+            <li><a class="menu-link main-nav-link ttu scroll-link" href="#ttu-container">Talk To Us</a></li>
+          </ul> <!-- /.menu -->
+        </div> <!-- /.inner -->
+      </div> <!-- /#overlay-menu -->
+    </nav> <!-- /#popout-nav /.container /.row -->
+    
+    <section id="portfolio-container">
+      <h1 id="logo-title">
+        <span class="title-text">Designated Developers</span>
+        <img id="logo" class="block-auto" src="/images/logo-watermark.png" alt="Watermark logo for Designated Developers">
+      </h1>
+      
+      <h2 id="portfolio-title" class="portfolio-top-text">We create <strong>amazing</strong> things.</h2>
+      <h3 id="portfolio-subhead" class="portfolio-top-text">See them in action.</h3>
+      
+      <a class="scroll-nav-link portfolio block-auto glyph scroll-link" href="#wwd-container" tabindex="6">&iacute;</a>
+      
+      <div id="portfolio-slider-wrapper">
+        <img id="portfolio-slider-bg" class="block-auto" src="/images/laptop.png" alt="A picture of a laptop, used to frame the slides">
+      </div> <!-- /#portfolio-slider-wrapper -->
+    </section> <!-- /#portfolio-container /.container -->
+    
+    <section id="wwd-container" class="container">
+      <div class="row">
+        <h2 id="wwd-title" class="column five section-title">What We Do</h2>
+        <div id="wwd-text" class="column right seven section-text">
+          <p>Maecenas suscipit nulla ligula, dapibus scelerisque lorem mattis et. Sed molestie neque eu dapibus interdum. Aenean sagittis, lorem molestie faucibus pretium, enim lacus pharetra mauris, at semper ipsum justo at arcu. Nulla pulvinar, dolor sit amet elementum varius, sem mauris vestibulum justo, eget consectetur dolor felis bibendum nunc. Curabitur id faucibus massa. Proin consequat orci ut lobortis consequat. Maecenas tristique mollis arcu, sed dignissim dui fermentum sit amet. Curabitur volutpat sodales aliquam. </p>
+          
+          <p>usce mollis laoreet tellus, vestibulum porttitor est bibendum ut. Nulla facilisi. Nulla nec urna id purus accumsan imperdiet. Donec vel ante sed lacus condimentum elementum vel sit. Curabitur mattis volutpat erat vitae vulputate. Donec congue orci massa, ut mollis velit euismod nec. In massa mauris, ullamcorper ac velit et, imperdiet ornare felis.</p>
+          
+          <a class="scroll-nav-link wwd block-auto glyph scroll-link" href="#wwa-container" tabindex="7">&iacute;</a>
+        </div> <!-- /#wwd-text /.column /.right /.seven /.section-text -->
+      </div> <!-- /.row -->
+    </section> <!-- /#wwd-container /.container -->
+    
+    <section id="wwa-container" class="container">
+      <img id="wwa-logo" class="block-auto" src="/images/logo-wwa.png" alt="Designated Developers logo in blue">
+      
+      <div id="wwa-bios" class="row">
+        <div class="column six">
+          <h4 class="wwa-heading">Who We Are</h4>
+          
+          <h2 class="wwa-name">Sara Beigle</h2>
+          
+          <p class="wwa-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque semper tellus, vel euismod sapien interdum mattis. Proin et arcu enim. Morbi auctor, libero in molestie posuere, nulla orci pulvinar ipsum, ut condimentum risus arcu in nisi. Pellentesque sed consequat neque, ac semper odio. Phasellus mattis est lectus, vitae hendrerit nunc hendrerit et. Curabitur volutpat sodales aliquam. Fusce mollis laoreet tellus, vestibulum porttitor est bibendum ut.</p>
+        </div> <!-- /.column /.six -->
+        <div class="column right six">
+          <h4 class="wwa-heading">Who We Are</h4>
+          
+          <h2 class="wwa-name">Thomas Beigle</h2>
+          
+          <p class="wwa-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque semper tellus, vel euismod sapien interdum mattis. Proin et arcu enim. Morbi auctor, libero in molestie posuere, nulla orci pulvinar ipsum, ut condimentum risus arcu in nisi. Pellentesque sed consequat neque, ac semper odio. Phasellus mattis est lectus, vitae hendrerit nunc hendrerit et. Curabitur volutpat sodales aliquam. Fusce mollis laoreet tellus, vestibulum porttitor est bibendum ut.</p>
+        </div> <!-- /.column /.right /.six -->
+      </div> <!-- /#wwa-bios /.row -->
+      
+      <div id="wwa-scroll-nav-link">
+        <a class="scroll-nav-link wwa block-auto glyph scroll-link" href="#ttu-container" tabindex="8">&iacute;</a>
+      </div> <!-- /#wwa-scroll-nav-link -->
+    </section> <!-- /#wwa-container /.container -->
+    
+    <section id="ttu-container" class="container">
+      <h2 id="ttu-title">Talk to Us</h2>
+      
+      <form name="contactForm" id="contact-form" class="block-auto" method="post" action="/includes/form-submit.php">
+        <div id="form-messages"></div> <!-- /#form-messages -->
+        <input type="text" name="name" id="field-name" class="textfield placeholder-label block-auto required" placeholder="What is your name?" tabindex="9">
+        <input type="email" name="email" id="field-email" class="textfield placeholder-label block-auto required" placeholder="Email address?" tabindex="10">
+        <input type="text" name="subject" id="field-subject" class="textfield placeholder-label block-auto required" placeholder="Subject" tabindex="1000">
+        <textarea name="message" id="field-message" class="placeholder-label block-auto required" placeholder="Message" tabindex="11"></textarea>
+        
+        <a id="field-submit" class="block-auto no-scroll form-submit" href="#contact-form" tabindex="12">send</a>
+      </form> <!-- /#contact-form -->
+    </section> <!-- /#ttu-container /.container -->
+    
+    <!-- JS files -->
+    <!--[if lt IE 9]>
+      <script type="text/javascript" src="/scripts/js/vendor/html5shiv.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="/scripts/js/vendor/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="/scripts/js/common-functions.js"></script>
+    <script type="text/javascript" src="/scripts/js/jquery.ddxmlslider.js"></script>
+    <script type="text/javascript" src="/scripts/js/main.js"></script>
   </body>
 </html>
