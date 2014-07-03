c5fa455ba0a8e3b40c600b73e9a1a14e26e96931
Tom Beigle <tbeigle@gmail.com>
2014-07-03 12:17:34 -0500

    More IE fixes.

--- docroot/scripts/js/lte-ie-9.js
+++ docroot/scripts/js/lte-ie-9.js
@@ -1,18 +1,29 @@
 (function($) {
-  $(document).ready(function() {
-    $('[placeholder]').focus(function() {
-      var $input = $(this);
+  $.fn.ieplaceholder = function(h) {
+    var $input = this,
+        ph_class = 'ie-placeholder';
+    
+    if (h == 'keydown' || ($input.val() != $input.attr('placeholder') && $input.val() != '')) {
+      $input.removeClass(ph_class);
       
       if ($input.val() == $input.attr('placeholder')) {
         $input.val('');
       }
+    }
+    else if ($input.val() == '') {
+      $input.addClass(ph_class).val($input.attr('placeholder'));
+    }
+  };
+  
+  $(document).ready(function() {
+    $('[placeholder]').keydown(function() {
+      $(this).ieplaceholder('keydown');
+    })
+    .keyup(function() {
+      $(this).ieplaceholder();
     })
     .blur(function() {
-      var $input = $(this);
-      
-      if ($input.val() == '') {
-        $input.val($input.attr('placeholder'));
-      }
+      $(this).ieplaceholder();
     })
     .blur();
   });
