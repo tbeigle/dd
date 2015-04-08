b747cab9986d32b5929bb5a23005c918cdbf31c6
Tom Beigle <tom@designated-developers.com>
2015-04-08 15:18:06 -0500

    DDI-6: Added positions to the bios.

--- docroot/sites/all/modules/custom/dd_people/dd-people-wwa.tpl.php
+++ docroot/sites/all/modules/custom/dd_people/dd-people-wwa.tpl.php
@@ -24,7 +24,8 @@
           <div class="<?php print $person->css_class; ?>">
             <h4 class="wwa-heading"><?php print $wwahead; ?></h4>
             
-            <h2 class="wwa-name"><?php print $person->name; ?></h2>
+            <h2 class="wwa-name"><?php print $person->name; ?><?php if (!empty($person->position)): ?><div class="wwa-heading"><?php print $person->position; ?></div><?php endif; ?></h2>
+            
             
             <div class="wwa-bio"><?php print $person->bio; ?></div>
             
